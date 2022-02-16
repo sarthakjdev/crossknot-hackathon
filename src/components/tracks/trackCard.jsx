21fdbf681e37278e1e93d27351ace65134cdaf81
sarthakjdev <jsarthak448@gmail.com>
2022-02-16 19:58:21 +0530

    fix: track page error fixed

--- src/components/tracks/trackCard.jsx
+++ src/components/tracks/trackCard.jsx
@@ -12,7 +12,7 @@ export default function TrackCard({ track}){
     return (
         <div className="flex bg-none border-card_dark border-8 rounded-md">
             <div className="flex flex-col items-center justify-center p-2 bg-card_light rounded-md m-2">
-                <Image src={track.trackIcon} alt={trackCard.trackName} />
+                <Image src={track.trackIcon} alt={track.trackName} />
                 <h4 className="text-center font-primary uppercase text-lg ">{track.trackName}</h4>
             </div>
         </div>
