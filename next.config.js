6d52098f740993063df9b6902bf1b457b405ace9
sarthakjdev <jsarthak448@gmail.com>
2022-02-19 17:44:16 +0530

    fix: added cors header in next config file

--- next.config.js
+++ next.config.js
@@ -1,11 +1,3 @@
 module.exports = {
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: '/',
-        destination: 'https://api.devfolio.co/:path*',
-      },
-    ]
-  },
 }
