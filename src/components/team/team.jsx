504111cee9d8370ad5bc3e51589786240ddda7ab
Aman Kumar <thisisamank@gmail.com>
2022-03-06 11:35:39 +0530

    Update team.jsx

--- src/components/team/team.jsx
+++ src/components/team/team.jsx
@@ -17,7 +17,8 @@ import teamHead from '../../../public/images/pageHeads/team.svg'
 
  export default function Teams(){
      return (
-         <section className="mx-20 mb-40">
+      {
+        /* <section className="mx-20 mb-40">
              <section className='flex flex-col my-10'>
              <SectionHead image={founderHead}/>
              <div className='flex flex-wrap items-center justify-center m-auto gap-20 gap-y-20 mt-4'> 
@@ -31,6 +32,8 @@ import teamHead from '../../../public/images/pageHeads/team.svg'
              }
              </div>
             </section>
+            */
+        }
             <section className='flex flex-col my-24'>
              <SectionHead image={teamHead}/>
              <div className='flex flex-wrap items-center justify-center m-auto gap-20 gap-y-40 mt-4'> 
