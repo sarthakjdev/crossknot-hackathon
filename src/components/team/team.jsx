c5813aebc40a01d0ae188761dc8040a4b2a2f4d2
Aman Kumar <thisisamank@gmail.com>
2022-03-06 11:39:24 +0530

    Update team.jsx

--- src/components/team/team.jsx
+++ src/components/team/team.jsx
@@ -17,23 +17,6 @@ import teamHead from '../../../public/images/pageHeads/team.svg'
 
  export default function Teams(){
      return (
-      
-        /* <section className="mx-20 mb-40">
-             <section className='flex flex-col my-10'>
-             <SectionHead image={founderHead}/>
-             <div className='flex flex-wrap items-center justify-center m-auto gap-20 gap-y-20 mt-4'> 
-             {
-                 teamsData.map((teamMember)=>{
-                     if(teamMember.memberDesignation === 'Founder' || teamMember.memberDesignation === 'Founder & Lead Organiser') {
-                        return (
-                            <TeamMemberCard key={teamMember.memberId} member={teamMember} />
-                        )}
-                    })
-             }
-             </div>
-            </section>
-            */
-        
             <section className='flex flex-col my-24'>
              <SectionHead image={teamHead}/>
              <div className='flex flex-wrap items-center justify-center m-auto gap-20 gap-y-40 mt-4'> 
