048c465b9d5c630411b4d420734be5e93346c490
Parth L <parth459@gmail.com>
2022-03-20 00:09:10 +0530

    test-1: Added space @eof

--- pages/team.js
+++ pages/team.js
@@ -1,29 +1,28 @@
-import Head from 'next/head'
-import TeamSection from "../src/components/team/team"
-import airtableBase from '../src/utils/airtable'
+import Head from "next/head";
+import TeamSection from "../src/components/team/team";
+import airtableBase from "../src/utils/airtable";
 
-export default function Team({team}){
-    return(
-            <>
-            <Head>
-            <title>Team | CrossKnot Hacks</title>
-            </Head>
-            <TeamSection team={team} />
-            </>
-    )
+export default function Team({ team }) {
+  return (
+    <>
+      <Head>
+        <title>Team | CrossKnot Hacks</title>
+      </Head>
+      <TeamSection team={team} />
+    </>
+  );
 }
 
-export async function getStaticProps(){
-
-    const data = await airtableBase('Team').select({maxRecords: 100,}).all()
-    let team = [];
-    data.forEach(member => {
-        team.push(member.fields)
-    });
-    return {
-        props: {
-            team: team
-        },
-        revalidate: 3600,
-    }
+export async function getStaticProps() {
+  const data = await airtableBase("Team").select({ maxRecords: 100 }).all();
+  let team = [];
+  data.forEach((member) => {
+    team.push(member.fields);
+  });
+  return {
+    props: {
+      team: team,
+    },
+    revalidate: 3600,
+  };
 }
