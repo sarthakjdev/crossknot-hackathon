0d834baf75e8da983c2e2e7f6c6d010f265b2885
sarthakjdev <jsarthak448@gmail.com>
2022-04-14 14:12:10 +0530

    feat: divided prize section into two categories prizes and sponsored track prizes

--- src/components/prizes/prizes.jsx
+++ src/components/prizes/prizes.jsx
@@ -13,19 +13,46 @@ export default function Prize({prizes}){
     const router = useRouter()
     return (
         <section className='flex flex-col mx-14 my-8 sm:mx-6' id='prizes'>
-            <SectionHead sectionName={'Prizes'} />
-            <div className='mt-9 flex justify-center item-center'>
-            </div>
-            <div className='grid grid-cols-3  items-center justify-evenly gap-8 midLg:grid-cols-1 rounded-xl py-20 m-auto mt-9 px-10 md:flex md:flex-wrap sm:px-0 sm:mt-4 sm:py-8'>
-                {   
+
+            <article className='flex flex-col'>
+                <SectionHead sectionName={'Prizes'} />
+                {/* <div className='mt-9 flex justify-center item-center'>
+                </div> */}
+                <div className='grid grid-cols-3  items-center justify-evenly gap-8 midLg:grid-cols-1 rounded-xl py-20 m-auto mt-9 px-10 md:flex md:flex-wrap sm:px-0 sm:mt-4 sm:py-8'>
+                    {   
                     prizes.map((prize)=>{
-                        return(
-                            <PrizeCard prizeData={prize} key={prize.id} />
-                        )
+                            if (prize.prizeType === 'normal') {
+                                return (
+                                    <PrizeCard prizeData={prize} key={prize.id} />
+                                )
+                            }
                     })
-                }
-            </div>
-            <SeeMoreButton url={'/prizes'} />
+                    }
+                </div>
+                {router.route === '/' && <SeeMoreButton url={'/prizes'}  />}
+            </article>
+
+
+            {
+                router.route === '/prizes' &&
+                <article className='flex flex-col'>
+                <SectionHead sectionName={'Sponsored Track Prizes'} />
+                {/* <div className='mt-9 flex justify-center item-center'>
+                </div> */}
+                <div className='grid grid-cols-3  items-center justify-evenly gap-8 midLg:grid-cols-1 rounded-xl py-20 m-auto mt-9 px-10 md:flex md:flex-wrap sm:px-0 sm:mt-4 sm:py-8'>
+                    {   
+                    prizes.map((prize)=>{
+                        if (prize.prizeType === 'track') {
+                            return(
+                                <PrizeCard prizeData={prize} key={prize.id} />
+                            )
+                        }
+                    })
+                    }
+                </div>
+            </article>
+            }
+
         </section>
     )
 }
